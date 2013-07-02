eed97c27cd9bab052b2ef672566056e66e835154
Strech (aka Sergey Fedorov) <strech_ftf@mail.ru>
2013-07-02 15:06:33 +0600

    feature(bosh) added authentication via sasl and non-sasl technology

---
+++ lib/jabber4r/bosh/authentication/sasl.rb
@@ -0,0 +1,239 @@
+# coding: utf-8
+
+# License: see LICENSE
+# Jabber4R - Jabber Instant Messaging Library for Ruby
+# Copyright (C) 2002  Rich Kilmer <rich@infoether.com>
+# Copyright (C) 2013  Sergey Fedorov <strech_ftf@mail.ru>
+
+require "ox"
+
+module Jabber::Bosh::Authentication
+  # This class provides SASL authentication for BOSH session
+  class SASL
+    attr_reader :session
+    attr_reader :mechanisms
+
+    def initialize(session)
+      @session = session
+    end
+
+    # Internal: Send open stream command to jabber server
+    #
+    # Raises XMLMalformedError
+    # Returns boolean
+    def open_stream
+      body = Ox::Element.new("body").tap do |element|
+        element[:xmlns]         = "http://jabber.org/protocol/httpbind"
+        element["xmlns:xmpp"]   = "urn:xmpp:xbosh"
+        element["xmpp:version"] = "1.0"
+        element[:content]       = "text/xml; charset=utf-8"
+        element[:rid]           = session.generate_next_rid
+        element[:to]            = session.domain
+        element[:secure]        = true
+        element[:wait]          = 60
+        element[:hold]          = 1
+      end
+
+      Jabber.debug(%Q[Open (rid="#{body.rid}") new session])
+
+      response = session.post(Ox.dump body)
+      xml = Ox.parse(response.body.tr("'", '"'))
+
+      define_stream_mechanisms(xml)
+
+      raise Jabber::XMLMalformedError,
+        "Couldn't find <body /> attribute [sid]" if xml[:sid].nil?
+
+      session.sid = xml.sid
+
+      true
+    end
+
+    # Internal: Send login request to jabber server
+    #
+    # password - String the password of jabber user
+    #
+    # Raises TypeError
+    # Raises XMLMalformedError
+    # Returns boolean
+    def pass_authentication(password)
+      # TODO : Define different exception type
+      raise TypeError,
+        "Server SASL mechanisms not include PLAIN mechanism" unless mechanisms.include?(:plain)
+
+      body = Ox::Element.new("body").tap do |element|
+        element[:xmlns]         = 'http://jabber.org/protocol/httpbind'
+        element["xmpp:version"] = "1.0"
+        element["xmlns:xmpp"]   = "urn:xmpp:xbosh"
+        element[:content]       = "text/xml; charset=utf-8"
+        element[:rid]           = session.generate_next_rid
+        element[:sid]           = session.sid
+
+        element << Query.new(session.jid, password, mechanism: :plain).to_ox
+      end
+
+      Jabber.debug(%Q[Authenticate {SASL} (rid="#{body.rid}" sid="#{body.sid}") in opened session] +
+                   %Q[ as #{session.jid.node}/#{session.jid.resource}])
+
+      response = session.post(Ox.dump body)
+      xml = Ox.parse(response.body.tr("'", '"'))
+
+      return false if xml.locate("success").empty?
+
+      true
+    end
+
+    def restart_stream
+      body = Ox::Element.new("body").tap do |element|
+        element[:xmlns]         = 'http://jabber.org/protocol/httpbind'
+        element["xmlns:xmpp"]   = "urn:xmpp:xbosh"
+        element["xmpp:version"] = "1.0"
+        element["xmpp:restart"] = true
+        element[:content]       = "text/xml; charset=utf-8"
+        element[:rid]           = session.generate_next_rid
+        element[:sid]           = session.sid
+        element[:to]            = session.jid.domain
+      end
+
+      response = session.post(Ox.dump body)
+      xml = Ox.parse(response.body.tr("'", '"'))
+
+      return false if xml.locate("stream:features/bind").empty?
+
+      true
+    end
+
+    def bind_resource
+      bind = Ox::Element.new("bind").tap do |element|
+        element[:xmlns] = "urn:ietf:params:xml:ns:xmpp-bind"
+
+        element << (Ox::Element.new("resource") << session.jid.resource)
+      end
+
+      iq = Ox::Element.new("iq").tap do |element|
+        element[:id]    = Jabber.gen_random_id
+        element[:type]  = "set"
+        element[:xmlns] = "jabber:client"
+
+        element << bind
+      end
+
+      body = Ox::Element.new("body").tap do |element|
+        element[:xmlns]         = 'http://jabber.org/protocol/httpbind'
+        element["xmlns:xmpp"]   = "urn:xmpp:xbosh"
+        element["xmpp:version"] = "1.0"
+        element[:content]       = "text/xml; charset=utf-8"
+        element[:rid]           = session.generate_next_rid
+        element[:sid]           = session.sid
+
+        element << iq
+      end
+
+      response = session.post(Ox.dump body)
+      xml = Ox.parse(response.body.tr("'", '"'))
+
+      raise Jabber::XMLMalformedError, "Couldn't find xml tag <iq/>" if xml.locate("iq").empty?
+      return false unless xml.iq[:type] == "result"
+
+      true
+    end
+
+    # TODO : Make state machine
+    def authenticate(jid, password)
+      open_stream
+
+      return false if pass_authentication(password) == false
+      return false if restart_stream == false
+
+      bind_resource
+    end
+
+    # Public: ...
+    #
+    # xml - Ox::Element
+    #
+    # Returns Array[Symbol]
+    def define_stream_mechanisms(xml)
+      @mechanisms = xml.locate("stream:features/mechanisms/mechanism/*")
+                       .map(&:downcase).map(&:to_sym)
+    end
+
+    private
+    class Query
+      # Full list of mechanisms
+      # http://www.iana.org/assignments/sasl-mechanisms/sasl-mechanisms.xhtml
+      MECHANISMS = [:plain].freeze
+
+      attr_reader :jid, :password
+      attr_reader :mechanism
+
+      # Public: Creates new SASL authentication object
+      #
+      # jid      - [Jabber::JID|String] the jid of jabber server user
+      # password - String the user password
+      # options  - Hash the authentication options (default: Empty hash)
+      #            :mechanism - Symbol the name of mechnism to use
+      #
+      # Examples
+      #
+      # non_sasl = Jabber::Protocol::Authentication::SASL.new("strech@localhost/res-1", "my-pass-phrase")
+      # non_sasl.plain? # => true
+      # non_sasl.to_xml # =>
+      #
+      # <auth xmlns="urn:ietf:params:xml:ns:xmpp-sasl" mechanism="PLAIN">biwsbj1qdWxpZXQscj1vTXNUQUF3QUFBQU1BQUFBTlAwVEFBQUFBQUJQVTBBQQ==</auth>
+      def initialize(jid, password, options = {})
+        raise TypeError,
+          "Class(Jabber::JID) or Class(String) expected," +
+          " but #{jid.class} was given" unless jid.is_a?(Jabber::JID) || jid.is_a?(String)
+
+        @jid = jid.is_a?(Jabber::JID) ? jid : Jabber::JID.new(jid)
+        @password = password
+
+        @mechanism = options.fetch(:mechanism, :plain)
+
+        raise ArgumentError,
+          "Unknown authentication mechanism '#{mechanism}'," +
+          " available is [#{MECHANISMS * ", "}]" unless MECHANISMS.include?(mechanism)
+      end
+
+      # Public: Is SASL object is for plain authentication
+      #
+      # Returns boolean
+      def plain?
+        mechanism == :plain
+      end
+
+      # Public: Create XML string from SASL object
+      #
+      # Returns String
+      def dump
+        Ox.dump(send mechanism)
+      end
+      alias :to_xml :dump
+
+      # Public: Create Ox::Element from SASL object
+      #
+      # Returns Ox::Element
+      def to_ox
+        send(mechanism)
+      end
+
+      private
+      # Internal: Make xml object for plain authentication mechanism
+      #
+      # Returns Ox:Element
+      def plain
+        Ox::Element.new("auth").tap do |element|
+          element[:xmlns]     = "urn:ietf:params:xml:ns:xmpp-sasl"
+          element[:mechanism] = "PLAIN"
+
+          element << self.class.generate_plain(jid, password)
+        end
+      end
+
+      def self.generate_plain(jid, password)
+        ["#{jid.strip}\x00#{jid.node}\x00#{password}"].pack("m").gsub(/\s/, "")
+      end
+    end
+  end
+end
---
+++ spec/lib/jabber4r/bosh/session_spec.rb
@@ -0,0 +1,406 @@
+# coding: utf-8
+require "spec_helper"
+
+describe Jabber::Bosh::Session do
+  before { described_class.any_instance.stub(:rand).and_return 1 }
+
+  describe "#bind" do
+    context "when server accept sasl authentication" do
+      let(:bosh_session) { described_class.bind("strech@localhost/resource", "password") }
+
+      let(:successful_login) { '<body xmlns="http://jabber.org/protocol/httpbind"><success xmlns="urn:ietf:params:xml:ns:xmpp-sasl"/></body>' }
+      let(:successful_open_stream) do
+        [
+          '<body xmlns="http://jabber.org/protocol/httpbind" xmlns:xmpp="urn:xmpp:xbosh" xmlns:stream="http://etherx.jabber.org/streams"' +
+          'charsets="UTF-8" from="localhost" hold="1" inactivity="20" polling="5" requests="2" sid="67d63e06-4159-43e2-9cb8-544de83eae58"' +
+          'ver="1.6" wait="60" xmpp:version="1.0"><stream:features><mechanisms xmlns="urn:ietf:params:xml:ns:xmpp-sasl"><mechanism>PLAIN</mechanism>' +
+          '</mechanisms></stream:features></body>'
+        ].join " "
+      end
+      let(:successful_restart) do
+        [
+          '<body xmlns="http://jabber.org/protocol/httpbind" xmlns:stream="http://etherx.jabber.org/streams">' +
+          '<stream:features><bind xmlns="urn:ietf:params:xml:ns:xmpp-bind"/></stream:features></body>'
+        ].join " "
+      end
+      let(:successful_bind) do
+        [
+          '<body xmlns="http://jabber.org/protocol/httpbind"><iq id="Jabber4R_f7e7fc2a10013070" type="result">' +
+          '<bind xmlns="urn:ietf:params:xml:ns:xmpp-bind"><jid>strech@localhost/resource</jid></bind></iq></body>'
+        ].join " "
+      end
+
+      let(:is_open_stream?) { ->(data) { x = Ox.parse(data); "2" == x[:rid] } }
+      let(:is_login?) { ->(data) { x = Ox.parse(data); "3" == x[:rid] && !x.locate("auth").empty? } }
+      let(:is_restart?) { ->(data) { x = Ox.parse(data); "4" == x[:rid] && x["xmpp:restart"] = "true" } }
+      let(:is_bind?) { ->(data) { x = Ox.parse(data); "5" == x[:rid] && !x.locate("iq/bind").empty? } }
+
+      context "when session successfuly binded" do
+        before do
+          stub_request(:post, "http://localhost:5280/http-bind")
+            .with(body: is_open_stream?)
+            .to_return(body: successful_open_stream)
+
+          stub_request(:post, "http://localhost:5280/http-bind")
+            .with(body: is_login?)
+            .to_return(body: successful_login)
+
+          stub_request(:post, "http://localhost:5280/http-bind")
+            .with(body: is_restart?)
+            .to_return(body: successful_restart)
+
+          stub_request(:post, "http://localhost:5280/http-bind")
+            .with(body: is_bind?)
+            .to_return(body: successful_bind)
+        end
+
+        it { expect(bosh_session).to be_alive }
+      end
+
+      context "when couldn't open stream" do
+        context "when response body missed sid attribute" do
+          let(:malformed_body) { '<body xmlns="http://jabber.org/protocol/httpbind" xmlns:xmpp="urn:xmpp:xbosh" xmlns:stream="http://etherx.jabber.org/streams"/>' }
+
+          before do
+            stub_request(:post, "http://localhost:5280/http-bind")
+              .with(body: is_open_stream?)
+              .to_return(body: malformed_body)
+
+            WebMock.should_not have_requested(:post, "http://localhost:5280/http-bind")
+              .with(body: is_login?)
+
+            WebMock.should_not have_requested(:post, "http://localhost:5280/http-bind")
+            .with(body: is_restart?)
+
+            WebMock.should_not have_requested(:post, "http://localhost:5280/http-bind")
+              .with(body: is_bind?)
+          end
+
+          it { expect { bosh_session }.to raise_error Jabber::XMLMalformedError, "Couldn't find <body /> attribute [sid]" }
+        end
+
+        context "when response was not 200 OK" do
+          before do
+            stub_request(:post, "http://localhost:5280/http-bind")
+              .with(body: is_open_stream?)
+              .to_return(body: "Foo", status: 401)
+
+            WebMock.should_not have_requested(:post, "http://localhost:5280/http-bind")
+              .with(body: is_login?)
+
+            WebMock.should_not have_requested(:post, "http://localhost:5280/http-bind")
+            .with(body: is_restart?)
+
+            WebMock.should_not have_requested(:post, "http://localhost:5280/http-bind")
+              .with(body: is_bind?)
+          end
+
+          it { expect { bosh_session }.to raise_error Net::HTTPBadResponse }
+        end
+      end
+
+      context "when couldn't login in opened stream" do
+        context "when response has no auth mechanisms" do
+          let(:malformed_body) do
+            [
+              '<body xmlns="http://jabber.org/protocol/httpbind" xmlns:xmpp="urn:xmpp:xbosh" xmlns:stream="http://etherx.jabber.org/streams"' +
+              'charsets="UTF-8" from="localhost" hold="1" inactivity="20" polling="5" requests="2" sid="67d63e06-4159-43e2-9cb8-544de83eae58"' +
+              'ver="1.6" wait="60" xmpp:version="1.0"/>'
+            ].join " "
+          end
+
+          before do
+            stub_request(:post, "http://localhost:5280/http-bind")
+              .with(body: is_open_stream?)
+              .to_return(body: malformed_body)
+
+            WebMock.should_not have_requested(:post, "http://localhost:5280/http-bind")
+            .with(body: is_login?)
+
+            WebMock.should_not have_requested(:post, "http://localhost:5280/http-bind")
+            .with(body: is_restart?)
+
+            WebMock.should_not have_requested(:post, "http://localhost:5280/http-bind")
+              .with(body: is_bind?)
+          end
+
+          it { expect { bosh_session }.to raise_error TypeError, "Server SASL mechanisms not include PLAIN mechanism" }
+        end
+
+        context "when response has type not equal 'success'" do
+          let(:malformed_body) { '<body xmlns="http://jabber.org/protocol/httpbind"><error xmlns="urn:ietf:params:xml:ns:xmpp-sasl"/></body>' }
+
+          before do
+            stub_request(:post, "http://localhost:5280/http-bind")
+              .with(body: is_open_stream?)
+              .to_return(body: successful_open_stream)
+
+            stub_request(:post, "http://localhost:5280/http-bind")
+              .with(body: is_login?)
+              .to_return(body: malformed_body)
+
+            WebMock.should_not have_requested(:post, "http://localhost:5280/http-bind")
+            .with(body: is_restart?)
+
+            WebMock.should_not have_requested(:post, "http://localhost:5280/http-bind")
+              .with(body: is_bind?)
+          end
+
+          it { expect { bosh_session }.to raise_error Jabber::AuthenticationError, "Failed to login" }
+        end
+
+        context "when response was not 200 OK" do
+          before do
+            stub_request(:post, "http://localhost:5280/http-bind")
+              .with(body: is_open_stream?)
+              .to_return(body: successful_open_stream)
+
+            stub_request(:post, "http://localhost:5280/http-bind")
+              .with(body: is_login?)
+              .to_return(body: "Foo", status: 401)
+
+            WebMock.should_not have_requested(:post, "http://localhost:5280/http-bind")
+            .with(body: is_restart?)
+
+            WebMock.should_not have_requested(:post, "http://localhost:5280/http-bind")
+              .with(body: is_bind?)
+          end
+
+          it { expect { bosh_session }.to raise_error Net::HTTPBadResponse }
+        end
+      end
+
+      context "when cann't restart opened stream" do
+        let(:malformed_body) { '<body xmlns="http://jabber.org/protocol/httpbind" xmlns:stream="http://etherx.jabber.org/streams"><error xmlns="urn:ietf:params:xml:ns:xmpp-sasl"/></body>' }
+
+        before do
+          stub_request(:post, "http://localhost:5280/http-bind")
+            .with(body: is_open_stream?)
+            .to_return(body: successful_open_stream)
+
+          stub_request(:post, "http://localhost:5280/http-bind")
+            .with(body: is_login?)
+            .to_return(body: successful_login)
+
+          stub_request(:post, "http://localhost:5280/http-bind")
+            .with(body: is_restart?)
+            .to_return(body: malformed_body)
+
+          WebMock.should_not have_requested(:post, "http://localhost:5280/http-bind")
+            .with(body: is_bind?)
+        end
+
+        it { expect { bosh_session }.to raise_error Jabber::AuthenticationError, "Failed to login" }
+      end
+
+      context "when cann't bind resource on opened stream" do
+        context "when response is a malformed xml without IQ tag" do
+          let(:malformed_body) { '<body xmlns="http://jabber.org/protocol/httpbind"></body>' }
+
+          before do
+            stub_request(:post, "http://localhost:5280/http-bind")
+              .with(body: is_open_stream?)
+              .to_return(body: successful_open_stream)
+
+            stub_request(:post, "http://localhost:5280/http-bind")
+              .with(body: is_login?)
+              .to_return(body: successful_login)
+
+            stub_request(:post, "http://localhost:5280/http-bind")
+              .with(body: is_restart?)
+              .to_return(body: successful_restart)
+
+            stub_request(:post, "http://localhost:5280/http-bind")
+              .with(body: is_bind?)
+              .to_return(body: malformed_body)
+          end
+
+          it { expect { bosh_session }.to raise_error Jabber::XMLMalformedError, "Couldn't find xml tag <iq/>" }
+        end
+
+        context "when response has type not equal 'result'" do
+          let(:malformed_body) { '<body xmlns="http://jabber.org/protocol/httpbind"><iq id="Jabber4R_f7e7fc2a10013070" type="error"/></body>' }
+
+          before do
+            stub_request(:post, "http://localhost:5280/http-bind")
+              .with(body: is_open_stream?)
+              .to_return(body: successful_open_stream)
+
+            stub_request(:post, "http://localhost:5280/http-bind")
+              .with(body: is_login?)
+              .to_return(body: successful_login)
+
+            stub_request(:post, "http://localhost:5280/http-bind")
+              .with(body: is_restart?)
+              .to_return(body: successful_restart)
+
+            stub_request(:post, "http://localhost:5280/http-bind")
+              .with(body: is_bind?)
+              .to_return(body: malformed_body)
+          end
+
+          it { expect { bosh_session }.to raise_error Jabber::AuthenticationError, "Failed to login" }
+        end
+      end
+    end
+
+    # REFACTOR ALL
+
+    context "when server accept non-sasl authentication" do
+      let(:bosh_session) { described_class.bind("strech@localhost/resource", "password", use_sasl: false) }
+
+      let(:successful_login) { '<body xmlns="http://jabber.org/protocol/httpbind"><iq type="result" xmlns="jabber:client" id="2034"/></body>' }
+      let(:successful_open_stream) do
+        [
+          '<body xmlns="http://jabber.org/protocol/httpbind" xmlns:xmpp="urn:xmpp:xbosh" xmlns:stream="http://etherx.jabber.org/streams"',
+          'authid="3780309894" sid="ce21410" from="localhost" xmpp:version="1.0"',
+          'wait="60" requests="2" inactivity="30" maxpause="120" polling="2" ver="1.8" secure="true" />'
+        ].join " "
+      end
+      let(:is_open_stream?) { ->(data) { x = Ox.parse(data); ["2", "localhost"] == [x[:rid], x[:to]] } }
+      let(:is_login?) { ->(data) { x = Ox.parse(data); ["3", "ce21410"] == [x[:rid], x[:sid]] } }
+
+      context "when session successfuly binded" do
+        before do
+          stub_request(:post, "http://localhost:5280/http-bind")
+            .with(body: is_open_stream?)
+            .to_return(body: successful_open_stream)
+
+          stub_request(:post, "http://localhost:5280/http-bind")
+            .with(body: is_login?)
+            .to_return(body: successful_login)
+        end
+
+        it { expect(bosh_session).to be_alive }
+      end
+
+      context "when couldn't open stream" do
+        context "when response body missed authid attribute" do
+          let(:malformed_body) { '<body xmlns="http://jabber.org/protocol/httpbind" sid="ce21410" />' }
+
+          before do
+            stub_request(:post, "http://localhost:5280/http-bind")
+              .with(body: is_open_stream?)
+              .to_return(body: malformed_body)
+
+            WebMock.should_not have_requested(:post, "http://localhost:5280/http-bind")
+              .with(body: is_login?)
+          end
+
+          it { expect { bosh_session }.to raise_error Jabber::XMLMalformedError, "Couldn't find <body /> attribute [authid]" }
+        end
+
+        context "when response body missed sid attribute" do
+          let(:malformed_body) { '<body xmlns="http://jabber.org/protocol/httpbind" authid="3780309894" />' }
+
+          before do
+            stub_request(:post, "http://localhost:5280/http-bind")
+              .with(body: is_open_stream?)
+              .to_return(body: malformed_body)
+
+            WebMock.should_not have_requested(:post, "http://localhost:5280/http-bind")
+              .with(body: is_login?)
+          end
+
+          it { expect { bosh_session }.to raise_error Jabber::XMLMalformedError, "Couldn't find <body /> attribute [sid]" }
+        end
+
+        context "when response was not 200 OK" do
+          before do
+            stub_request(:post, "http://localhost:5280/http-bind")
+              .with(body: is_open_stream?)
+              .to_return(body: "Foo", status: 401)
+
+            WebMock.should_not have_requested(:post, "http://localhost:5280/http-bind")
+              .with(body: is_login?)
+          end
+
+          it { expect { bosh_session }.to raise_error Net::HTTPBadResponse }
+        end
+      end
+
+      context "when couldn't login in opened stream" do
+        context "when response is a malformed xml without IQ tag" do
+          let(:malformed_body) { '<body xmlns="http://jabber.org/protocol/httpbind"></body>' }
+
+          before do
+            stub_request(:post, "http://localhost:5280/http-bind")
+              .with(body: is_open_stream?)
+              .to_return(body: successful_open_stream)
+
+            stub_request(:post, "http://localhost:5280/http-bind")
+              .with(body: is_login?)
+              .to_return(body: malformed_body)
+          end
+
+          it { expect { bosh_session }.to raise_error Jabber::XMLMalformedError, "Couldn't find xml tag <iq/>" }
+        end
+
+        context "when response has type not equal 'result'" do
+          let(:malformed_body) { '<body xmlns="http://jabber.org/protocol/httpbind"><iq type="error" xmlns="jabber:client" id="2034"/></body>' }
+
+          before do
+            stub_request(:post, "http://localhost:5280/http-bind")
+              .with(body: is_open_stream?)
+              .to_return(body: successful_open_stream)
+
+            stub_request(:post, "http://localhost:5280/http-bind")
+              .with(body: is_login?)
+              .to_return(body: malformed_body)
+          end
+
+          it { expect { bosh_session }.to raise_error Jabber::AuthenticationError, "Failed to login" }
+        end
+
+        context "when response was not 200 OK" do
+          before do
+            stub_request(:post, "http://localhost:5280/http-bind")
+              .with(body: is_open_stream?)
+              .to_return(body: successful_open_stream)
+
+            stub_request(:post, "http://localhost:5280/http-bind")
+              .with(body: is_login?)
+              .to_return(body: "Foo", status: 401)
+          end
+
+          it { expect { bosh_session }.to raise_error Net::HTTPBadResponse }
+        end
+      end
+    end
+  end
+
+  describe "#to_json" do
+    let(:bosh_session) { described_class.bind("strech@localhost/resource", "password", use_sasl: false) }
+    let(:successful_login) { '<body xmlns="http://jabber.org/protocol/httpbind"><iq type="result" xmlns="jabber:client" id="2034"/></body>' }
+    let(:successful_open_stream) do
+      [
+        '<body xmlns="http://jabber.org/protocol/httpbind" xmlns:xmpp="urn:xmpp:xbosh" xmlns:stream="http://etherx.jabber.org/streams"',
+        'authid="3780309894" sid="ce21410" from="localhost" xmpp:version="1.0"',
+        'wait="60" requests="2" inactivity="30" maxpause="120" polling="2" ver="1.8" secure="true" />'
+      ].join " "
+    end
+    let(:is_open_stream?) { ->(data) { x = Ox.parse(data); ["2", "localhost"] == [x[:rid], x[:to]] } }
+    let(:is_login?) { ->(data) { x = Ox.parse(data); ["3", "ce21410"] == [x[:rid], x[:sid]] } }
+
+    let(:json) { JSON.parse(bosh_session.to_json) }
+
+    before do
+      stub_request(:post, "http://localhost:5280/http-bind")
+        .with(body: is_open_stream?)
+        .to_return(body: successful_open_stream)
+
+
+      stub_request(:post, "http://localhost:5280/http-bind")
+        .with(body: is_login?)
+        .to_return(body: successful_login)
+    end
+
+    it { expect(json).to have_key "jid" }
+    it { expect(json).to have_key "rid" }
+    it { expect(json).to have_key "sid" }
+
+    it { expect(json["jid"]).to eq "strech@localhost/resource"}
+    it { expect(json["rid"]).to eq 3}
+    it { expect(json["sid"]).to eq "ce21410"}
+  end
+end
